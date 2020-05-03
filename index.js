7b9130ebf2652950b8d6b19e9c5cfebd5b73d732
Tara Z. Manicsic <tzmanics@gmail.com>
2020-05-02 22:58:47 -0400

    adds readdir filter and unlink to remove files

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
+const fs = require('fs');
 const glob = require('glob');
+const path = require('path');
 
 module.exports = {
-  onPostBuild: ({ constants: { PUBLISH_DIR } }) => {
+  onPostBuild: ({ constants: { PUBLISH_DIR }, utils }) => {
     console.log('🚮 Removing all the JS files.');
 
-    const jsFiles = glob.sync(`${PUBLISH_DIR}/**/*.js`);
-    console.log(jsFiles);
-  }
+    const files = fs
+      .readdirSync(PUBLISH_DIR)
+      .filter((file) => file.slice(-2) == 'js');
+
+    files.forEach((file) => {
+      fs.unlinkSync(`${PUBLISH_DIR}/${file}`, (error) => {
+        if (error) utils.build.failPlugin('unlinkSync error:', error);
+      });
+      console.log(`${file} has been deleted 💣`);
+    });
+
+    const builtPages = glob.sync(`${PUBLISH_DIR}/*`);
+    console.log(builtPages);
+  },
 };
