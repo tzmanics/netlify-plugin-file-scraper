0d0332344defdfaaadd423d67f73ec6fe0c442ee
Tara Z. Manicsic <tzmanics@gmail.com>
2020-05-04 23:19:10 -0400

    implements recursive searches on all dirs + gitignore

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 const fs = require('fs');
 const path = require('path');
+const readdirp = require('readdirp');
 
 module.exports = {
-  onPostBuild: ({ constants: { PUBLISH_DIR }, inputs, utils }) => {
+  onPostBuild: async ({ constants: { PUBLISH_DIR }, inputs, utils }) => {
+    const fileTypeLength = inputs.fileType.length;
+    let files = [];
+
     if (inputs.fileType == '') return console.log('🤦🏻‍♀️ Oops, need a file type.');
+
     console.log(`🚮 Removing all the ${inputs.fileType} files.`);
 
-    const fileTypeLength = inputs.fileType.length;
-    const files = fs
-      .readdirSync(PUBLISH_DIR)
-      .filter((file) => file.slice(-fileTypeLength) == inputs.fileType);
+    for await (const entry of readdirp(PUBLISH_DIR, {
+      fileFilter: `*.${inputs.fileType}`,
+    })) {
+      files.push(entry.path);
+    }
 
-    files.forEach((file) => {
-      fs.unlinkSync(`${PUBLISH_DIR}/${file}`, (error) => {
-        if (error) utils.build.failPlugin('unlinkSync error:', error);
+    files
+      .filter((file) => file.slice(-fileTypeLength) == inputs.fileType)
+      .forEach((file) => {
+        fs.unlinkSync(`${PUBLISH_DIR}/${file}`, (error) => {
+          if (error) utils.build.failPlugin('unlinkSync error:', error);
+        });
+        console.log(`${file} has been deleted 💣`);
       });
-      console.log(`${file} has been deleted 💣`);
-    });
   },
 };
